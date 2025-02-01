792b4452dbf2d17b14bb7ac38b59289aafb563ae
M3hTi <mehdinasir502@yahoo.com>
2025-02-01 13:33:24 +0330

    feat: implement country search functionality with data fetching and state management

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,98 @@ import * as React from 'react'
 
 import './App.css'
 
+
+function reducer({state,action}) {
+  switch (action.type) {
+    case 'LOADING':
+      return {info: {}, isLoading: true, isError: false}
+    case 'LOADED':
+      return {info: action.payload, isLoading: false, isError: false}
+    case 'ERROR':
+      return {info : {}, isLoading: false, isError: action.payload}
+    default:
+      return state
+  }
+}
+
+
+
+
 function App() {
+  const initialInformation = {
+    info: [],
+    isLoading: false,
+    isError: false
+  }
+
+  const [term, setTerm] = React.useState('')
+  const [confirmSearch, setConfirmSearch] = React.useState('')
+
+  const[data, dispatchData] = React.useReducer(reducer, initialInformation)
+
+
+  const handlerInput = React.useCallback((e) => {
+    const newValue = e.target.value 
+    setTerm(newValue)
+  },[term])
+  
+
+  const handleConfirmSrch = React.useCallback((e) => {
+    if(e.type=== 'click' || (e.type === 'keydown' && e.key === 'Enter')){
+      setConfirmSearch(term)
+    }
+  },[term])
+
+
+  const fetchData = React.useCallback(() => {
+    if(confirmSearch === '') return 
+      dispatchData({type: 'LOADING'})
+    fetch(`https://restcountries.com/v3.1/name/${confirmSearch}`)
+      .then(response => {
+        if(!response.ok) throw new Error("Error is occurred");
+        return response.json()
+      })
+      .then(data => dispatchData({type: 'LOADED', payload: data}))
+      .catch(error => {
+        dispatchData({type: 'ERROR', payload: error.message})
+      });
+  },[confirmSearch])
+
+
+  React.useEffect(() => {
+    fetchData()
+  },[fetchData])
+
+
+
+
 
   return (
     <>
-      
+      <h1>REST Countries</h1>
+      <InputWithButton id='search' type='text' value={term} onHandler={handlerInput} onConfirm={handleConfirmSrch} />
+      <div>
+        {data.isError && <p>{data.isError}</p>}
+        {data.isLoading ? (<div className="spinner"></div>) : (<List items={data.info} />)}
+      </div>
     </>
   )
 }
+
+function InputWithButton({id, type, value, onHandler, onConfirm}) {
+  return(
+    <>
+      <input type={type} id={id} value={value} onInput={onHandler} onKeyDown={onConfirm}  placeholder='Please enter the country name' />
+      <button onClick={onConfirm}>Search</button>
+    </>
+  )
+}
+
+
+
+function List({items}) {
+  console.log(items);
+}
+
 
 export default App
